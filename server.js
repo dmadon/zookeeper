367e3045c29e0aeac277d7b1aa05f5847faad30e
dmadon <deanna.madon@gmail.com>
2022-08-01 20:42:54 -0500

    add POST functionality that allows users to create a new animal and add it to the database

--- server.js
+++ server.js
@@ -1,15 +1,13 @@
 const express = require('express');
 const PORT = process.env.PORT||3001;
 const app = express();
+// parse incoming string or array data
+app.use(express.urlencoded({extended:true}));
+// parse incoming JSON data
+app.use(express.json());
 const {animals} = require('./data/animals.json');
-
-
-
-
-
-
-
-
+const fs = require('fs');
+const path = require('path');
 
 
 function filterByQuery(query, animalsArray){
@@ -57,7 +55,37 @@ function filterByQuery(query, animalsArray){
 function findById(id, animalsArray){
     const result = animalsArray.filter(animal => animal.id === id)[0];
     return result;
-}
+};
+
+function createNewAnimal(body,animalsArray){
+    console.log(body);
+    // function's main code:
+    const animal = body;
+    animalsArray.push(animal)
+    fs.writeFileSync(
+        path.join(__dirname,'./data/animals.json'),
+        JSON.stringify({animals: animalsArray}, null, 2)
+    );
+
+    // return finished code to post route for response
+    return animal;
+};
+
+function validateAnimal(animal){
+    if(!animal.name || typeof animal.name!=='string'){
+        return false;
+    }
+    if(!animal.species || typeof animal.species!=='string'){
+        return false;
+    }
+    if(!animal.diet || typeof animal.diet!=='string'){
+        return false;
+    }
+    if(!animal.personalityTraits || !Array.isArray(animal.personalityTraits)){
+        return false;
+    }
+    return true;
+};
 
 
 app.get('/api/animals',(req,res)=>{
@@ -77,6 +105,31 @@ app.get('/api/animals/:id', (req, res) => {
         }
       
   });
+
+app.post('/api/animals',(req,res) => {
+    // req.body is where our incoming content will be
+    // set id based on what the next index of the array will be
+    req.body.id = animals.length.toString();
+
+    // if any data in req.body is incorrect, send 400 error back
+    if(!validateAnimal(req.body)){
+        res.status(400).send("This animal's data is not properly formatted. Please ensure that all properties are completed with the correct data type.");
+    }
+    else{
+        const animal = createNewAnimal(req.body, animals);
+        res.json(animal);
+    }
+
+    // add animal to json file and animals array in this function
+    // const animal = createNewAnimal(req.body, animals);
+
+    // console.log(req.body);
+    // res.json(req.body);
+})
+
+
+
+
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
 });
